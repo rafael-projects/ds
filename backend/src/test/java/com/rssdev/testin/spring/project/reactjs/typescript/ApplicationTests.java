e1d75b4a2cea1938996247215bada1df8975f921
rafael-projects <rss4110@hotmail.com>
2022-01-14 04:00:50 -0300

    Created project

---
+++ backend/src/test/java/com/rssdev/testin/spring/project/reactjs/typescript/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rssdev.testin.spring.project.reactjs.typescript;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
